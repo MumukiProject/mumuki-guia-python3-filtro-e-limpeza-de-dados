895f68cc37e0c5ad1ae92a2dd886e5704ad770fb
franco@mumuki.org <franco@mumuki.org>
2022-10-19 12:47:10 +0000

    Mumuki Export on 2022-10-19 12:47:10 +0000

---
+++ 00005_Não sou eu, são _y_ e _o_/hint.md
@@ -0,0 +1,7 @@
+A sintaxe da disjunção lógica é a mesma que a da conjunção:
+
+```python
+tabela[uma_condição | outra_condição]
+```
+
+E novamente, lembre-se de que, se a condição usar operadores, ela deverá ser colocada entre parênteses.
---
+++ 00005_Não sou eu, são _y_ e _o_/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Test(unittest.TestCase):
+  def setUp(self):
+    self.resultado = (
+      #...content...#
+    )
+  
+  def test_gera_o_DataFrame(self):
+    self.assertEquals(type(self.resultado), pd.DataFrame)
+    
+  def test_retorna_apenas_salgueiros(self):
+    self.assertEquals(len(self.resultado), 3)
+    
+    self.assertTrue(8087 in list(self.resultado["tree_id"]), "Deveria ter encontrado a árvore 8087")
+    self.assertTrue(142527 in list(self.resultado["tree_id"]), "Deveria ter encontrado a árvore 142527")
+    self.assertTrue(182465 in list(self.resultado["tree_id"]), "Deveria ter encontrado a árvore 182465")
