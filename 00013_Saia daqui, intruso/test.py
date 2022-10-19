7d1979dd0c814d3f1be298036dd7a4fc1ef438f9
franco@mumuki.org <franco@mumuki.org>
2022-10-19 16:20:44 +0000

    Mumuki Export on 2022-10-19 16:20:44 +0000

--- 00013_Saia daqui, intruso/test.py
+++ 00013_Saia daqui, intruso/test.py
@@ -1,9 +1,9 @@
 class Test(unittest.TestCase):
 
-  def test_arboles_deberia_tener_ahora_menos_filas(self):
-    self.assertTrue(len(arboles) == 9, "Debería haber menos árboles en el DataFrame original")
+  def test_arvores_agora_devem_ter_menos_linhas(self):
+    self.assertTrue(len(arvores) == 9, "Deve haver menos árvores no DataFrame original")
     
-  def test_deberian_haberse_ido_arboles_demasiado_altos_y_anchos(self):
-    self.assertTrue(182465 not in arboles.set_index("tree_id").index)
+  def test_árvores_muito_altas_e_largas_deveriam_ter_sido_removidas(self):
+    self.assertTrue(182465 not in arvores.set_index("tree_id").index)
       
-    
+        
