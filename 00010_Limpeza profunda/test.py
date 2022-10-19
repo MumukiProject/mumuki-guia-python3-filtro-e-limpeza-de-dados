0f5734491b3818530a05a9d06103fe6dcdd1f67c
franco@mumuki.org <franco@mumuki.org>
2022-10-19 16:04:09 +0000

    Mumuki Export on 2022-10-19 16:04:09 +0000

--- 00010_Limpeza profunda/test.py
+++ 00010_Limpeza profunda/test.py
@@ -1,9 +1,9 @@
 class Test(unittest.TestCase):
 
   def test_as_árvores_com_colunas_NAN_são_removidas(self):
-    self.assertTrue(len(arboles) == 7, "todas as árvores com valores nan deveriam ter sido removidas")
+    self.assertTrue(len(arvores) == 7, "todas as árvores com valores nan deveriam ter sido removidas")
     
-    indexado = arboles.set_index("tree_id")
+    indexado = arvores.set_index("tree_id")
     
     self.assertTrue(4101876 not in indexado, "todas as árvores com valores nan deveriam ter sido removidas")
     self.assertTrue(4101166 not in indexado, "todas as árvores com valores nan deveriam ter sido removidas")
