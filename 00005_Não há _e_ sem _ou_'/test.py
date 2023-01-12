8690b732b6eb9dc2ff3c122e733b4daa24b5f1b7
franco@mumuki.org <franco@mumuki.org>
2023-01-12 15:50:23 +0000

    Mumuki Export on 2023-01-12 15:50:23 +0000

--- 00005_Não há _e_ sem _ou_'/test.py
+++ 00005_Não há _e_ sem _ou_'/test.py
@@ -9,7 +9,7 @@ class Test(unittest.TestCase):
   def test_gera_o_DataFrame(self):
     self.assertEquals(type(self.resultado), pd.DataFrame)
     
-  def test_retorna_apenas_salgueiros(self):
+  def test_contém_apenas_as_árvores_solicitadas(self):
     self.assertEquals(len(self.resultado), 3)
     
     self.assertTrue(8087 in list(self.resultado["tree_id"]), "Deveria ter encontrado a árvore 8087")
